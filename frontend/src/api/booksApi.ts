27059a22c670d2f12d95d27ae2be2c7f2e1bd40c
Damjan Zimbakov <damjan@hubbyesim.com>
2026-06-29 12:28:55 +0300

    fix/auth-issue-fixed

--- frontend/src/api/booksApi.ts
+++ frontend/src/api/booksApi.ts
@@ -7,19 +7,20 @@ import {
   PostBodyCreateBookDto,
   GetBooksParams,
 } from "@/dto/BooksDto";
+import { useCallback, useMemo } from "react";
 
 const useBooksService = () => {
   const { Get, Post } = useRootApiService();
 
-  const getBooks = async (
+  const getBooks = useCallback(async (
     params?: GetBooksParams
   ): Promise<ApiResponseDto<GetBooksResponseDto>> => {
     const response = await Get<GetBooksResponseDto>("/books", params);
 
     return response;
-  };
+  }, [Get]);
 
-  const createBook = async (
+  const createBook = useCallback(async (
     data: PostBodyCreateBookDto
   ): Promise<ApiResponseDto<BookResponseDto>> => {
     const response = await Post<BookResponseDto, PostBodyCreateBookDto>(
@@ -28,9 +29,9 @@ const useBooksService = () => {
     );
 
     return response;
-  };
+  }, [Post]);
 
-  const deleteBooksBatch = async (
+  const deleteBooksBatch = useCallback(async (
     data: DeleteBooksBodyBatchDto
   ): Promise<ApiResponseDto<MessageDto>> => {
     const response = await Post<MessageDto, DeleteBooksBodyBatchDto>(
@@ -39,9 +40,12 @@ const useBooksService = () => {
     );
 
     return response;
-  };
+  }, [Post]);
 
-  return { getBooks, createBook, deleteBooksBatch };
+  return useMemo(
+    () => ({ getBooks, createBook, deleteBooksBatch }),
+    [getBooks, createBook, deleteBooksBatch]
+  );
 };
 
 export default useBooksService;
